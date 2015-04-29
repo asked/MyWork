7015a6a089a3a781712041a088ee1ae4c40ff1d5
Nader Yafi <asked@live.co.uk>
2015-04-29 03:39:14 -0700

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -1,19 +1,13 @@
 import urlparse
 import urllib
-import sys
-import re 
+from sys import argv
 from bs4 import BeautifulSoup 
-def my_emaillist(homepage, textfile , eamilstext,HTMLfileout ):
+def my_emaillist(homepage, textfile ):
 
-	urlfile = open(textfile,'a')  
-	emailsfile = open(eamilstext,'a')  
-	HTMLfile = open(HTMLfileout,'a')
+	urlfile = open(textfile,'a')   
 	url = homepage
 	urls = [url] 
 	visited =  [url]
-	eamils =[]
-	visitedemail=[]
-	wholetext = []
 	while len(urls) > 0  :
 		try :
 			htmltext = urllib.urlopen (urls[0]).read()
@@ -21,16 +15,16 @@ def my_emaillist(homepage, textfile , eamilstext,HTMLfileout ):
 			print urls[0]
 		soup = BeautifulSoup(htmltext)
 		urls.pop(0)
-		eamils = re.compile( '\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}')
-		visitedemail.append("".join( re.findall (eamils,str(soup))))	
 		for tag in soup.findAll('a',href= True):
 			tag['href']= urlparse.urljoin(url ,tag['href'])
 			if url in tag['href'] and tag['href'] not in visited:
 				urls.append(tag['href'])
 				visited.append(tag['href'])
-			
-	urlfile.write(str(visited))	
-	emailsfile.write(str(visitedemail))
+	for i in visited:	
+		urlfile.write(str(i+"\n"))	
 	urlfile.close()
-	emailsfile.close()
-my_emaillist("http://www.mileycyrus.com","file.txt","eamils.txt","HTMLPage.txt")
+	
+	
+if __name__ == "__main__":
+	script, url = argv
+	my_emaillist(url,"file.txt",)
